7a8590016a45fb0c330e3345211c3c768c9aa943
Eric Fritz <eric@eric-fritz.com>
2020-04-12 13:23:51 -0500

    Initial commit.

---
+++ .buildkite/index-go.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+OUTPUT=`mktemp -d -t sgdockerbuild_XXXXXXX`
+cleanup() {
+    rm -rf "$OUTPUT"
+}
+trap cleanup EXIT
+
+# TODO - add to build agent instead
+go get github.com/sourcegraph/lsif-go/cmd/lsif-go
+go get github.com/sourcegraph/src-cli/cmd/src
+
+git clone $1 $OUTPUT
+cd $OUTPUT
+lsif-go
+src-cli lsif upload

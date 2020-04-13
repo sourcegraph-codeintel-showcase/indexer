b71e70f7fd3306c94ce2dbe9093ddeadd1cbfe05
Eric Fritz <eric@eric-fritz.com>
2020-04-13 11:12:43 -0500

    Update scripts to index multiple repos.

--- .buildkite/index-go.sh
+++ .buildkite/index-go.sh
@@ -2,13 +2,15 @@
 
 set -ex
 
-OUTPUT=`mktemp -d -t sgdockerbuild_XXXXXXX`
-cleanup() {
-    rm -rf "$OUTPUT"
-}
-trap cleanup EXIT
+for repo in "$@"; do
+    OUTPUT=`mktemp -d -t sgdockerbuild_XXXXXXX`
+    cleanup() {
+        rm -rf "$OUTPUT"
+    }
+    trap cleanup EXIT
 
-git clone "https://$1.git" "$OUTPUT"
-cd "$OUTPUT"
-lsif-go
-SRC_ACCESS_TOKEN=$CODE_INTEL_SHOWCASE_SOURCEGRAPH_ACCESS_TOKEN src lsif upload
+    git clone "https://$repo.git" "$OUTPUT"
+    cd "$OUTPUT"
+    lsif-go
+    SRC_ACCESS_TOKEN=$CODE_INTEL_SHOWCASE_SOURCEGRAPH_ACCESS_TOKEN src lsif upload
+done
